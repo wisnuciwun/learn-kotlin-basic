c7920f521f77c1804f68867bc008b20b4b84f6a8
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-14 11:04:26 +0700

    learn part 3 - number and operators

---
+++ app/src/main/java/com/learnkotlin/basic/NumberAndOperator.kt
@@ -0,0 +1,12 @@
+package com.learnkotlin.basic
+
+fun main() {
+    val a = 12
+    val b = 25
+
+    println(a + b)
+    println(a - b)
+    println(a / b)
+    println(a * b)
+    println(b % a)
+}
